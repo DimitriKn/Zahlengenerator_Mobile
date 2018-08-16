2cbece97e3e9f3a5a39abb8f92ea784d1b5441fb
Dimitri Knippenberg <dimitri.knippenberg@gmx.net>
2018-08-16 20:32:17 +0200

    import Project

---
+++ Numbgen_mobile/Numbgen_mobile/MainPage.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Numbgen_mobile
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            Erg.Text = "Zufallszahlengenerator. Geben Sie die Zahlen von 1 bis 1000";
+            Erg.FontSize = 16;
+
+           
+        }
+       
+    private void Gen6x49_Clicked(object sender, EventArgs e)
+        {
+            Rand(6, 49);
+        }
+
+        private void GenByClick_Clicked (object sender, EventArgs e)
+        {
+            try
+            {
+                int lg = Convert.ToInt32(Eing1.Text);
+                int kg = Convert.ToInt32(Eing2.Text);
+
+                if (lg == 0 || kg > 1000)
+                {
+                    Erg.Text = "nur von 1 bis 1000";
+                   
+                    Erg.FontSize = 16;
+
+                }
+
+                else
+                {
+
+                    Rand(lg, kg);
+                }
+            }
+            catch (Exception ex)
+            {
+                Erg.Text = ex.Message;
+             
+                Erg.FontSize = 16;
+            }
+        }
+
+        private void Gen5x50ByClick_Clicked (object sender, EventArgs e)
+        {
+            Rand(5, 50);
+        }
+
+       
+
+
+        private void Rand(int lg, int kg)
+        {
+            Random rnd = new Random();
+
+            StringBuilder sb = new StringBuilder();
+
+            if (lg == 5 && kg == 50)
+            {
+                string[] numbs = new string[lg];
+
+                for (int ctr = 0; ctr < lg; ctr++)
+                {
+                    numbs[ctr] = rnd.Next(1, kg).ToString();
+                }
+
+                foreach (var num in numbs.OrderBy(x => int.Parse(x)))
+                {
+                    sb.Append(num).Append(" ");
+                }
+
+                sb.Append("   ");
+
+
+                string[] numbs2 = new string[2];
+
+                for (int tr = 0; tr < 2; tr++)
+                {
+                    numbs2[tr] = rnd.Next(1, 10).ToString();
+                }
+
+                foreach (var num in numbs2.OrderBy(x => int.Parse(x))) // zahlen anordnen
+                {
+                    sb.Append(num).Append(" ");
+                }
+
+                string res = sb.ToString();
+                Erg.Text = res;
+               
+            }
+            else
+            {
+                // string array initialesieren
+                string[] numbs = new string[lg];
+                // befüllen
+                for (int ctr = 0; ctr < lg; ctr++)
+                {
+                    numbs[ctr] = rnd.Next(1, kg).ToString();
+                }
+                //ausgeben 
+                foreach (var num in numbs.OrderBy(x => int.Parse(x)))
+                {
+                    sb.Append(num).Append(" ");
+                }
+
+                string res = sb.ToString();
+                Erg.Text = res;
+               
+            }
+        }
+    }
+}
